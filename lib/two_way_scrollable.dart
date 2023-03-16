679ba957d945f21fc4ea3ac456ee106330dbaefc
Alex Isaienko <thesonerik@gmail.com>
2023-03-16 21:30:02 +0200

    export sliver_two_way_list_view.dart

--- lib/two_way_scrollable.dart
+++ lib/two_way_scrollable.dart
@@ -1,3 +1,5 @@
+export 'src/list_view/sliver_two_way_list_view.dart'
+    hide SliverItemsSectionState;
 export 'src/list_view/two_way_list_view.dart';
 export 'src/list_view/two_way_list_view_controller.dart'
     hide RemovedItemBuilder;
